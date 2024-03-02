c5a339a06b2e352f842e882180265c47fc5782c9
mzsweeting <116032509+mzsweeting@users.noreply.github.com>
2024-03-02 11:20:57 -0500

    updated GitHub link

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
         <a
           href="https://github.com/mzsweeting/react-forecast-app.git"
           target="_blank"
+          rel="noopener noreferrer"
         >
           open-sourced on github
         </a>
