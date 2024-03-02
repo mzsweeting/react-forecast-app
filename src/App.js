e1d2d7950a6a87154962ba4360c0694888fbd8e1
mzsweeting <116032509+mzsweeting@users.noreply.github.com>
2024-03-02 10:51:39 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by: Anntoinette Sweeting and is{" "} 
+        <a
+          href="https://github.com/mzsweeting/react-forecast-app.git"
+          target="_blank"
+        >
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+ );
 }
 
